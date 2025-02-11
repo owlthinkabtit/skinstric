315658de2766fa3229bb4c7efda2fd9a95fc0f77
owlthinkabtit <haggins.jameka@gmail.com>
2025-02-11 16:23:40 -0500

    working on Phase one as talked about over our last meeting. Started with the header, just wanted to get everything synced up correctly with github.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <header className="header">
+      skinstric
+      <button className="code">enter code</button>
+     </header>
+    </div>
+  );
+}
+
+export default App;
