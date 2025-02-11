6fea5094c57aebf502a478677445ea43e3787b3e
owlthinkabtit <haggins.jameka@gmail.com>
2025-02-11 16:59:47 -0500

    I think this is the correct font, I had to find it on a free site, since google fonts didn't have the one I was looking for. There's so differences but hopefully this will suffice.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <header className="header">
-      skinstric
-      <button className="code">enter code</button>
+      <div className="logo">skinstric</div>
+      <button className="code-btn">enter code</button>
      </header>
+      <main className="main-content">
+        <h1 className="headline">Sophisticated 
+          <br></br>skincare</h1>
+      </main>
     </div>
   );
 }
